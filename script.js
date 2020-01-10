a4727d5756153f4356fd8b9acc71152628d85a1b
Ben Hsu <benbhsu@users.noreply.github.com>
2020-01-09 20:09:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,57 @@
-$(document).ready(function(){
-        // iOS web app full screen hacks.
-        if(window.navigator.standalone == true) {
-                // make all link remain in web app mode.
-                $('a').click(function() {
-                        window.location = $(this).attr('href');
-            return false;
-                });
+static getIOSScope(): string {
+    const standalone = (window.navigator as any).standalone;
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    const safari = /safari/.test(userAgent);
+    const ios = /iphone|ipod|ipad/.test(userAgent);
+    if (ios) {
+        if (!standalone && safari) {
+            return 'browser';
+        } else if (standalone && !safari) {
+            return 'standalone';
+        } else if (!standalone && !safari) {
+            return 'uiwebview';
         }
-});
+    }
+    return 'another';
+}
+
+static fixIOSLinks() {
+    const scope = Utils.getIOSScope();
+    if (scope !== 'standalone' && scope !== 'uiwebview') {
+        return;
+    }
+    // _blank links open inside
+    const links = document.getElementsByTagName('a');
+    for (let i = 0; i < links.length; ++i) {
+        if (links[i].getAttribute('target') == '_blank') {
+            links[i].removeAttribute('target');
+            links[i].setAttribute('data-target', '_blank');
+        }
+    }
+    // not _blank links open outside => iOS is piece of shit
+    document.addEventListener('click', function (e) {
+        let element: Element = e.target as Element;
+        while (!/^(a|html)$/i.test(element.nodeName) && element.parentNode) {
+            element = element.parentNode as Element;
+        }
+        if (element.getAttribute) {
+            const href = element.getAttribute('href');
+            const target = element.getAttribute('data-target');
+            if (target) {
+                return;
+            }
+            const protocol = (element as any).protocol;
+            const isHrefDefined = !!href && '#' !== href;
+            if (isHrefDefined) {
+                const isWebProtocol = !protocol || protocol !== 'tel:';
+                const isRelativePath = Utils.isRelativePath(href);
+                const pattern = new RegExp("^[a-z][a-z0-9+.-]*:\/\/" + window.document.location.host);
+                const isCurrentHost = pattern.test(href);
+                if (isWebProtocol && (isRelativePath || isCurrentHost)) {
+                    e.preventDefault();
+                    window.document.location.assign(href);
+                }
+            }
+        }
+    }, false);
+}
